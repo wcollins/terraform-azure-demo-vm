c2950d0c3a30b9fd631f02a3e7af8676fbccef7a
wcollins <william.levi.collins@gmail.com>
2021-01-20 16:28:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "snet" {
   name                 = var.snet_name
-  address_prefixes     = [var.address_prefixes]
+  address_prefixes     = [var.snet_prefixes]
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
